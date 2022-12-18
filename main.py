5ffd41390ea0629a9125cb8c6cd93e29f1e3e00e
pencase65 <pneu65rk377@inorikka.com>
2022-12-18 17:06:48 +0900

    引数を処理するところを別ファイルに分けてみた

--- main.py
+++ main.py
@@ -1,16 +1,8 @@
-import argparse
 import acquire
-
-
-def get_args():
-    psr = argparse.ArgumentParser()
-    psr.add_argument('-i', '--channel_id', help='Enter your YouTube channel ID')
-
-    return psr.parse_args()
-
+import args
 
 if __name__ == "__main__":
-    args = get_args()
+    args = args.get_args()
     url = acquire.acquire(args.channel_id)
     print(args.channel_id)
     print(url)
